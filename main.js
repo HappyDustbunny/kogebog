dd57144ce12005483a8634392c4b32c0b98892a8
Mads Horn <madshorn@operamail.com>
2020-11-19 22:16:00 +0100

    Fix accordion and some layout

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ $(function() {
   $('.ingredienser').hide();
   $('.howto').hide();
 
+  $('.unfold').on('click', function() {
+    $('.opskrifter').slideToggle();
+    $('.ingredienser').slideToggle();
+    $('.howto').slideToggle();
+  })
+
   $('.oversigt').on('click', '.kategori', function(event) {
     event.preventDefault();
     $(this).next('.opskrifter').not('animated').slideToggle();
