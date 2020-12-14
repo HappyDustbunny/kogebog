8fe8c2ed5e6b4de1219bc4c72040ce042bd71bfd
Mads Horn <madshorn@operamail.com>
2020-12-14 17:46:13 +0100

    Duplicate sidedishes to unfold where needed

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ $(function() {
   $('.opskrifter').hide();
   $('.ingredienser').hide();
   $('.howto').hide();
+  // Repeted recipes are treated differently: they do not unfold until explicitly clicked
+  $('.insertedIngredienser').hide();
+  $('.insertedHowto').hide();
+  // The a link is shown and the about button is hidden if no javascript is present
   $('#fallBackLink').hide();
   $('#about').show();
   $('#about').on('click', function() {
     window.location = 'index.html'
   });
-
 
 
   $('#foldOutFoldIn').on('click', function() {
@@ -22,11 +25,12 @@ $(function() {
 
   });
 
+
   let direction = 1;
   let originalFontSize = parseInt($('body').css("font-size"));
   let fontSize = originalFontSize;
   let step = 5;
-  $('#textSize').on('click', function() {  // Cycle between fontsizes
+  $('#textSize').on('click', function() {  // Cycle between fontsizes normal-big-biggest-big-normal-big-...
     fontSize += direction * step;
     if (fontSize == originalFontSize + 2 * step) {
       direction = -1;
@@ -42,20 +46,25 @@ $(function() {
     }
   });
 
+
   function hide() {
     $('.opskrifter').hide();
     $('.ingredienser').hide();
     $('.howto').hide();
+    $('.insertedIngredienser').hide();
+    $('.insertedHowto').hide();
 
     $('#foldOutFoldIn')[0].value = 'unfold';
     $('#foldOutFoldIn')[0].textContent = 'Fold index ud';
   }
+
 
   function showIndex() {
     $('.opskrifter').show();
     $('#foldOutFoldIn')[0].value = 'unfoldAll';
     $('#foldOutFoldIn')[0].textContent = 'Fold alt ud (for at kunne s\u00f8ge)';
   }
+
 
   function show() {
     $('.opskrifter').show();
@@ -66,10 +75,12 @@ $(function() {
     $('#foldOutFoldIn')[0].textContent = 'Fold alt ind';
   }
 
+  // Fold everything
   $('.slut').on('click', function() {
     $('input').prop("checked", false);
     hide();
   });
+
 
   $('.oversigt').on('click', '.kategori', function(event) {
     event.preventDefault();
@@ -80,5 +91,12 @@ $(function() {
     event.preventDefault();
     $(this).next('.ingredienser').not('animated').slideToggle();
     $(this).next('.ingredienser').next('.howto').not('animated').slideToggle();
+  });
+
+  // Repeted recipes are only unfolded when clicked explicitly
+  $('.insertedRecipe').on('click', function(event) {
+    event.preventDefault();
+    $(this).next('.insertedIngredienser').not('animated').slideToggle();
+    $(this).next('.insertedIngredienser').next('.insertedHowto').not('animated').slideToggle();
   });
 })
